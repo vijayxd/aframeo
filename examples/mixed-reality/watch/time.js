a62956381f5ad5971490004cb8cacee9715e3afb
vijayxd <vijayxd@gmail.com>
2024-03-13 12:50:50 +0530

    Delete examples directory

--- examples/mixed-reality/watch/time.js
+++
@@ -1,9 +0,0 @@
-/* global AFRAME */
-AFRAME.registerComponent('time', {
-  tick: function () {
-    var date = new Date();
-    this.el.setAttribute('text', 'value',
-      date.getHours().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false}) +
-      ':' + date.getMinutes().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false}));
-  }
-});
